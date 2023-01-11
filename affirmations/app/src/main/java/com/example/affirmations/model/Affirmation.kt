23530980c911c0adab2e330622c457c739a7437d
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2023-01-11 11:27:26 +0000

    Add display a list of images using cards

--- affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,4 +1,10 @@
 package com.example.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 // pass in the string resource ID when creating an affirmation
-data class Affirmation(val stringResourceId: Int)
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+    )
